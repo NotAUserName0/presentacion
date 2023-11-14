0ef0df545c7aed343003a878d1d195a68859fd93
NotAUserName0 <dill2897@gmail.com>
2023-11-14 13:15:05 -0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+
+import Reveal from 'reveal.js'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'demoD';
+
+  constructor() {
+  }
+
+  ngAfterViewInit(){
+    Reveal.initialize({
+      controls:false,
+      history:true,
+      touch:true,
+      transition: 'concave',
+    })
+
+    Reveal.addEventListener('fragmentshown',event=>{
+      console.log(event.fragment.dataset.fragmentIndex)
+    });
+  }
+
+  siguiente(){
+    Reveal.next()
+  }
 }
