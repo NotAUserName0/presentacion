bfcb3472ab0974a392f16828875f58d80a61508f
NotAUserName0 <dill2897@gmail.com>
2023-11-16 16:38:30 -0600

    continue 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,13 +26,19 @@ import Swal from "sweetalert2";
 })
 export class AppComponent implements AfterViewInit{
   title = 'demoD';
-  private audio: HTMLAudioElement;
+  private audios: HTMLAudioElement[] = [];
   //pop dia1
   pop1:boolean = false
   pop2:boolean = false
+  pop3:boolean = false
+  //pop dia 2
+  pop4:boolean = false
 
   constructor() {
-    this.audio = new Audio("assets/1.mp3")
+    for (let i = 1; i <= 4; i++) { //agrego audios
+      const audio = new Audio(`assets/${i}.mp3`);
+      this.audios.push(audio);
+    }
   }
 
   ngAfterViewInit(){
@@ -49,15 +55,25 @@ export class AppComponent implements AfterViewInit{
 
       console.log("slide changed to: "+ evento['indexh'] + "vertical: " + evento['indexv'])
 
-      //Diapositiva 1
+      //initial - reinicia audios
+      for (let i = 1; i <= 4; i++){
+        this.audios[i].pause()
+        this.audios[i].currentTime = 0
+      }
+
+      //animacion de botones
+      document.getElementById("anim").classList.add("rebote")
+      document.getElementById("anim1").classList.add("rebote")
+
+      //Diapositiva 1 - audio 0
       if(evento['indexh']===1 ){
-        this.audio.volume = 0.2
-        this.audio.play()
-      }else{
-        this.audio.pause()
-        if(evento['indexv']!==1){
-          this.audio.currentTime = 0
-        }
+        this.audios[0].volume = 0.2
+        this.audios[0].play()
+      }
+      //Diapositiva 2 - audio 1
+      if(evento['indexh']===2 ){
+        this.audios[1].volume = 0.2
+        this.audios[1].play()
       }
 
 
@@ -66,7 +82,29 @@ export class AppComponent implements AfterViewInit{
         //console.log(event.fragment.dataset.fragmentIndex)
         console.log("Presentacion: "+evento['indexh']+", Fragmento: "+event.fragment.dataset.fragmentIndex)
 
+        if(evento['indexh']===2){ //fragmentos de 2
+          //initial
+          this.audios[1].pause()
+          this.audios[1].currentTime = 0
+          if(event.fragment.dataset.fragmentIndex === "0"){
+            this.audios[1].pause()
+            this.audios[1].currentTime = 0
+            console.log("entrada1")
+            this.audios[2].volume = 0.2
+            this.audios[2].play()
+          }
+          if(event.fragment.dataset.fragmentIndex === "1"){
+            this.audios[2].pause()
+            this.audios[2].currentTime = 0
+            console.log("entrada2")
+            this.audios[3].volume = 0.2
+            this.audios[3].play()
+          }
+        }
       });
+
+      
+
 
     })
 
@@ -77,8 +115,17 @@ export class AppComponent implements AfterViewInit{
     Reveal.slide(indexh, indexv);
   }
 
+  goToMainSlide(indexh){
+    Reveal.slide(indexh)
+    
+  }
+
   siguiente(){
     Reveal.next()
+  }
+
+  anterior(){
+    Reveal.prev()
   }
 
   pause(){
