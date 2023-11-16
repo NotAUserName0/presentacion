ec07c95536b2bd121c389b3ce559f141c0f47edc
Fermin Jimenez <dill2897@gmail.com>
2023-11-15 21:32:04 -0600

    pop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component} from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
 import Reveal from 'reveal.js'
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-root',
@@ -18,8 +19,13 @@ import Reveal from 'reveal.js'
 })
 export class AppComponent implements AfterViewInit{
   title = 'demoD';
+  private audio: HTMLAudioElement;
+  //pop dia1
+  pop1:boolean = false
+  pop2:boolean = false
 
   constructor() {
+    this.audio = new Audio("assets/1.mp3")
   }
 
   ngAfterViewInit(){
@@ -29,46 +35,60 @@ export class AppComponent implements AfterViewInit{
       history:true,
       touch:true,
       transition: 'concave',
-      autoSlide: false
+      autoSlide: false,
     })
 
     Reveal.addEventListener('slidechanged', evento=>{ //evento de cambio de slide
 
-      console.log("slide changed to: "+ evento['indexh'])
-      
-      if(evento['indexh']===1){
-        
+      console.log("slide changed to: "+ evento['indexh'] + "vertical: " + evento['indexv'])
+
+      //Diapositiva 1
+      if(evento['indexh']===1 ){
+        this.audio.volume = 0.2
+        //this.audio.play()
+      }else{
+        this.audio.pause()
+        if(evento['indexv']!==1){
+          this.audio.currentTime = 0
+        }
       }
+
 
       //console.log('Evento slidechanged h:', event['indexh']);
       Reveal.addEventListener('fragmentshown',event=>{ //al entrar
         //console.log(event.fragment.dataset.fragmentIndex)
         console.log("Presentacion: "+evento['indexh']+", Fragmento: "+event.fragment.dataset.fragmentIndex)
 
-        
       });
 
     })
 
-    Reveal.addEventListener('slidehidden',event=>{
-      
-    })
 
-
-  }
-
-  siguiente(){
-    Reveal.next()
-  }
-
-  atras(){
-    Reveal.prev()
   }
 
   goToSlide(indexh, indexv) {
     Reveal.slide(indexh, indexv);
   }
 
+  siguiente(){
+    Reveal.next()
+  }
+
+  pause(){
+
+  }
+
+  reanuda(){
+
+  }
+
+  noClose(event: MouseEvent){ //General
+    event.stopPropagation()
+  }
+
+  msg(){
+    alert("Click presionado")
+  }
 
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SweetAlert2Module.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
