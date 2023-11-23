58b6bfe69770b673c9b141c30e88353b83a35021
NotAUserName0 <dill2897@gmail.com>
2023-11-23 14:04:04 -0600

    newDocs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component} from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
 import Reveal from 'reveal.js'
@@ -23,51 +23,44 @@ import Reveal from 'reveal.js'
     ]),
   ]
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements AfterViewInit {
   title = 'demoD';
   private audios: HTMLAudioElement[] = [];
   //pop dia1
-  pop1:boolean = false
-  pop2:boolean = false
-  pop3:boolean = false
+  pop1: boolean = false
+  pop2: boolean = false
+  pop3: boolean = false
   //pop dia 2
-  pop4:boolean = false
+  pop4: boolean = false
 
   constructor() {
-    for (let i = 1; i <= 5; i++) { //agrego audios
+    for (let i = 1; i <= 12; i++) { //agrego audios
       const audio = new Audio(`assets/${i}.m4a`);
       this.audios.push(audio);
     }
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
+    document.getElementById("home")?.classList.add("dontShow")
 
     Reveal.initialize({
-      controls:false,
-      history:true,
-      touch:true,
+      controls: false,
+      history: true,
+      touch: true,
       transition: 'concave',
       autoSlide: false,
       sync: true
     })
 
-    Reveal.addEventListener('slidechanged', evento=>{ //evento de cambio de slide
+    Reveal.addEventListener('slidechanged', evento => { //evento de cambio de slide
 
-      console.log("slide changed to: "+ evento['indexh'] + "vertical: " + evento['indexv'])
+      console.log("slide changed to: " + evento['indexh'] + "vertical: " + evento['indexv'])
       Reveal.sync()
+      document.getElementById("home")?.classList.remove("dontShow")
 
       //initial - reinicia audios
-      /*this.audios[0].pause()
-      this.audios[0].currentTime = 0
-      this.audios[1].pause()
-      this.audios[1].currentTime = 0
-      this.audios[2].pause()
-      this.audios[2].currentTime = 0
-      this.audios[3].pause()
-      this.audios[3].currentTime = 0*/
-
-      for(let i = 0; i < 5 ; i++){
-        if(this.audios[i].played){
+      for (let i = 0; i < 12; i++) {
+        if (this.audios[i].played) {
           this.audios[i].currentTime = 0
           this.audios[i].pause()
         }
@@ -76,45 +69,83 @@ export class AppComponent implements AfterViewInit{
       //animacion de botones
       for (let i = 0; i < 10; i++) {
         document.getElementById("anim" + i)?.classList.add("rebote");
-    }
-    
+      }
 
+
+      if (evento['indexh'] === 0) {
+        document.getElementById("home")?.classList.add("dontShow")
+      }
       //Diapositiva 1 - audio 0
-      if(evento['indexh']===1 ){
+      if (evento['indexh'] === 1) {
         this.audios[0].volume = 0.2
         this.audios[0].play()
       }
       //Diapositiva 2 - audio 1
-      if(evento['indexh']===2 ){
+      if (evento['indexh'] === 2) {
         this.audios[1].volume = 0.2
         this.audios[1].play()
       }
 
       //Diapositiva 3 - audio 4
-      if(evento['indexh']===3 ){
+      if (evento['indexh'] === 3) {
         this.audios[4].volume = 0.2
         this.audios[4].play()
       }
 
+      if (evento['indexh'] === 4) {
+        this.audios[5].volume = 0.2
+        this.audios[5].play()
+      }
+
+      if (evento['indexh'] === 5) {
+        this.audios[6].volume = 0.2
+        this.audios[6].play()
+      }
+
+
+      if (evento['indexh'] === 6) {
+        this.audios[7].volume = 0.2
+        this.audios[7].play()
+      }
+
+      if (evento['indexh'] === 7) {
+        this.audios[8].volume = 0.2
+        this.audios[8].play()
+      }
+
+      if (evento['indexh'] === 8) {
+        this.audios[9].volume = 0.2
+        this.audios[9].play()
+      }
+
+      if (evento['indexh'] === 9) {
+        this.audios[10].volume = 0.2
+        this.audios[10].play()
+      }
+
+      if (evento['indexh'] === 10) {
+        this.audios[11].volume = 0.2
+        this.audios[11].play()
+      }
 
       //console.log('Evento slidechanged h:', event['indexh']);
-      Reveal.addEventListener('fragmentshown',event=>{ //al entrar
+      Reveal.addEventListener('fragmentshown', event => { //al entrar
         //console.log(event.fragment.dataset.fragmentIndex)
-        console.log("Presentacion: "+evento['indexh']+", Fragmento: "+event.fragment.dataset.fragmentIndex)
+        console.log("Presentacion: " + evento['indexh'] + ", Fragmento: " + event.fragment.dataset.fragmentIndex)
 
-        if(evento['indexh']===2){ //fragmentos de diapositiva 2
+        if (evento['indexh'] === 2) { //fragmentos de diapositiva 2
           //audios 2, 3
           //initial
           this.audios[1].pause()
           this.audios[1].currentTime = 0
-          if(event.fragment.dataset.fragmentIndex === "0"){
+          if (event.fragment.dataset.fragmentIndex === "0") {
             this.audios[1].pause()
             this.audios[1].currentTime = 0
             console.log("entrada1")
             this.audios[2].volume = 0.2
             this.audios[2].play()
           }
-          if(event.fragment.dataset.fragmentIndex === "1"){
+          if (event.fragment.dataset.fragmentIndex === "1") {
             this.audios[2].pause()
             this.audios[2].currentTime = 0
             console.log("entrada2")
@@ -136,32 +167,32 @@ export class AppComponent implements AfterViewInit{
     Reveal.slide(indexh, indexv);
   }
 
-  goToMainSlide(indexh, indexv, fragment){
-    Reveal.slide(indexh, indexv,fragment);
+  goToMainSlide(indexh, indexv, fragment) {
+    Reveal.slide(indexh, indexv, fragment);
 
   }
 
-  siguiente(){
+  siguiente() {
     Reveal.next()
   }
 
-  anterior(){
+  anterior() {
     Reveal.prev()
   }
 
-  control(id){
-    if(!this.audios[id].paused){
+  control(id) {
+    if (!this.audios[id].paused) {
       this.audios[id].pause()
-    }else{
+    } else {
       this.audios[id].play()
     }
   }
 
-  noClose(event: MouseEvent){ //General
+  noClose(event: MouseEvent) { //General
     event.stopPropagation()
   }
 
-  msg(){
+  msg() {
     alert("Click presionado")
   }
 
